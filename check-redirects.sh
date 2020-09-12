3163ff9bc4a0412e59641f45928b13ab255f6f6b
Curtis Rueden <ctrueden@wisc.edu>
2020-09-12 00:27:59 -0500

    check-redirects.sh: ignore commented lines

--- check-redirects.sh
+++ check-redirects.sh
@@ -3,6 +3,7 @@ file=$1
 domain=$2
 cat "$file" | while read line
 do
+  test "$line" = "${line#\#}" || continue
   old=${line% *}
   new=${line#* }
   result=$(curl -Is "https://$domain$old" | grep '^Location: ' | sed 's/^Location: //' | tr -d '\r')
